74f77fd2c9b6f2d047e25235ec4c33319f349de1
Dhiraj Madhukar Jagtap <dhirajdj30@gmail.com>
2022-03-10 08:40:06 +0530

    Both the files are commited

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String[] args) {
         int a = 4;
-        int b = 2;
+        int b = 8;
         System.out.println("the addition is "+(a+b));
     }
 }
